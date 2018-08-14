6c2c8bee906d8f8f0436619a1d9738bb69d68ffd
Niels Cölle <niels@coelle-online.com>
2018-08-14 05:52:58 +0200

    Project created because Playground has bugs

---
+++ GenericTableViewController/GenericTableViewController/EpisodesViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GenericTableViewController
+//
+//  Copyright © 2018 Cölle Online. All rights reserved.
+//
+
+import UIKit
+
+struct Episode {
+	var title: String
+}
+
+class EpisodesViewController: UITableViewController {
+	let reuseIdentifier = "Cell"
+
+	var episodes: [Episode] = []
+
+	override func viewDidLoad () {
+		episodes = [
+			Episode(title: "First Episode"),
+			Episode(title: "Second Episode"),
+			Episode(title: "Third Episode")
+		]
+		tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+	}
+
+	override func tableView (_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return episodes.count
+	}
+
+	override func tableView (_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+		let item = episodes[indexPath.row]
+		cell.textLabel?.text = item.title
+		return cell
+	}
+}
